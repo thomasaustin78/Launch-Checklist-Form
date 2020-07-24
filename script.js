b255faff0dd2b5985bbc14293efc4cc5dde6bd1a
Austin Thomas <thomasaustin78@gmail.com>
2020-07-23 20:51:27 -0500

    added function to make jsonArray

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      return response.json()
+   }).then(function(jsonArray){
+      console.log(jsonArray);
+   })
    let form = document.querySelector("launchForm");
       launchForm.addEventListener("submit", function(event) {
          let pilotNameInput = document.getElementById("pilotName");
@@ -45,3 +49,15 @@ window.addEventListener("load", function() {
             event.preventDefault();
    });
 });
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${}</li>
+   <li>Diameter: ${}</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${}">
+*/
